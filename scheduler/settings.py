715cf29d42f5fe15125d2e6364cda0b2f3e5e37c
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-09-06 20:11:07 +0200

    add dotenv

--- scheduler/settings.py
+++ scheduler/settings.py
@@ -11,17 +11,23 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dotenv
 from pathlib import Path
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w=$d(m-k1wo=cmx#c=x4w^*2v&ui_z%j-crzry68&)*ao$3b15'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,7 +45,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
-    # 'django_celery_beat',
     'django_celery_results'
 ]
 
