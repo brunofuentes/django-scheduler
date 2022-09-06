34bbc5b283f0ebe369f45110758a7aa663c2e18b
Bruno V. Fuentes <brunofuentess@gmail.com>
2022-09-06 22:27:28 +0200

    fix requester, start_on

--- app/models.py
+++ app/models.py
@@ -13,10 +13,10 @@ class Request(models.Model):
 
     id = models.BigAutoField(primary_key=True)
     title = models.CharField(max_length=100)
-    requester = models.ForeignKey(User, default=User.objects.first().id, on_delete=models.CASCADE)
+    requester = models.ForeignKey(User, on_delete=models.CASCADE)
     type = models.CharField(choices=REQUEST_TYPES ,default="GET", max_length=100)
     url = models.CharField(null=False, default='' ,max_length=500)
-    start_on = models.DateTimeField(default=datetime.now(), null=False)
+    start_on = models.DateTimeField(null=False)
     status = models.TextField(max_length=50, null=True)
     res_status = models.TextField(max_length=500)
     res_body = models.JSONField(default=dict)
